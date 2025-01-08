18a30b6e2867cd203919e44804492454e08e2f93
Atharva Dharmadhikari <37585719+Atharva01@users.noreply.github.com>
2025-01-08 16:54:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-from app.ner_app import perform_ner
-
+import subprocess
+subprocess.run(
+    ["python3","huggingface_login.py"]
+)
+from service.service import NER
 text = """
 Microsoft is headquartered in Redmond, Washington. Satya Nadella is the CEO.
 """
 
-entities = perform_ner(text)
+ner = NER(text)
+entities = ner.perform_ner()
 for entity in entities:
     print(f"Entity: {entity['word']}, Label:{entity['entity_group']}, Confidence: {entity['score']:.2f}")
