83c42d03b375eb3c7ff210532f1c910e8df822e6
Atharva Dharmadhikari <37585719+Atharva01@users.noreply.github.com>
2025-01-08 17:11:19 +0100

    Update huggingface_login.py

--- huggingface_login.py
+++ huggingface_login.py
@@ -1,21 +1,33 @@
 import subprocess
-# Replace 'your_token_here' with your Hugging Face token
-token = 'your_token_here'
+import argparse
 
-def configure_huggingface_token(token):
-    """
-    Configure Hugging Face CLI with a token programmatically.
-    :param token: Hugging Face access token as a string.
-    """
-    try:
-        # Set the token in Hugging Face CLI configuration
-        subprocess.run(
-            ["huggingface-cli", "login", "--token", token]
-        )
-        print("Login successful! Hugging Face CLI is now configured.")
-    except subprocess.CalledProcessError as e:
-        print("Error during login:")
-        print(e.stderr)
+class Login:
+    def __init__(self):
+        pass
 
-configure_huggingface_token(token)
+    def configure_huggingface_token(self,token):
+        """
+        Configure Hugging Face CLI with a token programmatically.
+        :param token: Hugging Face access token as a string.
+        """
+        self.token = token
+        try:
+            # Set the token in Hugging Face CLI configuration
+            subprocess.run(
+                ["huggingface-cli", "login", "--token", self.token]
+                )
+            print("Login successful! Hugging Face CLI is now configured.")
+        except subprocess.CalledProcessError as e:
+            print("Error during login:")
+            print(e.stderr)
 
+
+def main():
+    parser = argparse.ArgumentParser(description="Hugging Face login thorugh CLI")
+    parser.add_argument('--token',type=str,help='Your Hugging Face Access Token')
+    args = parser.parse_args()
+    login = Login()
+    login.configure_huggingface_token(args.token)
+
+if __name__ == "__main__":
+    main()
